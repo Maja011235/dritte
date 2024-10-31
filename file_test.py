ec05ab8ee291437a0c79eec4be25b0e949869843
Maja <maja.shaul@freenet.de>
2024-10-31 09:21:49 +0100

    Add pytest and flake8 workflow

---
+++ file_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_calc_addition():
+    # Testet das Ergebnis von 2+4
+    output = 2 + 4
+    assert output == 6
+
+def test_calc_subtraction():
+    # Testet das Ergebnis von 2-4
+    output = 2 - 4
+    assert output == -2
